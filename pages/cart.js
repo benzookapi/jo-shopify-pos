0bbc11fd06b04f48f7caba3930f6fbdc1d171235
Junichi Okamura <junichi.okamura@shopify.com>
2019-09-12 03:55:46 +0900

    3

--- pages/cart.js
+++ pages/cart.js
@@ -7,6 +7,26 @@ const Cart = () => (
     <p>You can edit this page in "/pages/cart</p>
     <p>For information on Next.js dynamic routing <a href="https://nextjs.org/learn/basics/navigate-between-pages" target="_blank">check out their documentation</a></p>
     <p>For information about navigation within the admin frame, <a href="https://help.shopify.com/en/api/embedded-apps/app-extensions/navigation/create-navigation-link" target="_blank">see the Shopify documentation.</a></p>
+  <script>
+    var iframe = document.createElement('iframe');
+    var start = new Date().getTime();
+    iframe.style.border = 'none';
+    iframe.style.width = '1px';
+    iframe.style.height = '1px';
+
+    iframe.src = 'twitter://post?message=' + encodeURIComponent('ほげほげ #ふがふが');
+    document.body.appendChild(iframe);
+
+    setTimeout(function() {
+    var diff = new Date().getTime() - start;
+
+    if (diff < 510) {
+    window.location.href = 'http://itunes.com/apps/twitter';
+    iframe.parentNode.removeChild(iframe);
+    }
+    }, 500);
+  </script>
+  
   </Page>
 )
 export default Cart;
